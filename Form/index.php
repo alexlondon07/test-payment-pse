c9c3776cb0869db66384cb2bf788f6b968fd8e8f
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-06-23 14:48:00 -0500

    create form basic...

---
+++ Form/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Alexander Londoño</title>
+  <link rel="stylesheet" href="lib/bootstrap.css" crossorigin="anonymous">
+  <script src="lib/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <form role="form">
+      <div class="form-group">
+        <label for="document">document</label>
+        <input type="text" class="form-control" id="document" name="document" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="firstName">firstName</label>
+        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="documentType">documentType</label>
+        <input type="text" class="form-control" id="documentType" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="document">document</label>
+        <input type="text" class="form-control" id="document" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="emailAddress">emailAddress</label>
+        <input type="text" class="form-control" id="emailAddress" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="address">address</label>
+        <input type="text" class="form-control" id="address" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="city">city</label>
+        <input type="text" class="form-control" id="city" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="province">province</label>
+        <input type="text" class="form-control" id="province" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="country">country</label>
+        <input type="text" class="form-control" id="country" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="phone">phone</label>
+        <input type="text" class="form-control" id="phone" placeholder="">
+      </div>
+      <div class="form-group">
+        <label for="mobile">mobile</label>
+        <input type="text" class="form-control" id="mobile" placeholder="">
+      </div>
+      <button type="submit" class="btn btn-default">Enviar</button>
+    </form>
+  </div>
+</body>
+</html>
