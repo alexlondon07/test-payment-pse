bbc7a93e8360d9da363a340ec7392af7206b408f
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-06-26 11:12:31 -0500

    class Person √

---
+++ Form/classes/Person.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Description of Person
+ * Clase para reflejar la información de una persona involucrada en una transacción
+ * @author Alexander Londoño Espejo.
+ */
+class Person {
+    /**
+     *
+     * @var string $documentType
+     */
+    public $documentType;
+
+    /**
+     *
+     * @var string $document
+     */
+    public $document;
+
+    /**
+     *
+     * @var string $firstName
+     */
+    public $firstName;
+
+    /**
+     *
+     * @var string $lastName
+     */
+    public $lastName;
+
+    /**
+     *
+     * @var string $company
+     */
+    public $company;
+
+    /**
+     *
+     * @var string $emailAddress
+     */
+    public $emailAddress;
+
+    /**
+     *
+     * @var string $address
+     */
+    public $address;
+
+    /**
+     *
+     * @var string $city
+     */
+    public $city;
+
+    /**
+     *
+     * @var string $province
+     */
+    public $province;
+
+    /**
+     *
+     * @var string $country
+     */
+    public $country;
+
+    /**
+     *
+     * @var string $phone
+     */
+    public $phone;
+
+    /**
+     *
+     * @var string $mobile
+     */
+    public $mobile;
+
+}
