aebb05c8b48e05acd86d981c85aad7e9b97d19c7
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-06-26 11:02:20 -0500

    class Buyer √

---
+++ Form/classes/Buyer.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Description of class Buyer
+ * Información del comprador
+ * @author Alexander Londoño Espejo.
+ */
+class Buyer {
+    public $documentType;
+    public $document;
+    public $firstName;
+    public $lastName;
+    public $company;
+    public $emailAddress;
+    public $address;
+    public $city;
+    public $province;
+    public $country;
+    public $phone;
+    public $mobile;
+}
